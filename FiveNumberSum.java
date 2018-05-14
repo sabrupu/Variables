196cb3d43933fb17a4159c7c560d1be1b37c093f
Sabrina Gill <39250291+sabrupu@users.noreply.github.com>
2018-05-14 15:35:43 -0400

    Add files via upload

---
+++ FiveNumberSum.java
@@ -0,0 +1,42 @@
+public class FiveNumberSum {
+    public static void main(String[] args){
+
+        int number1 = 10                      ;
+        int number2 = 20                      ;
+        int number3 = 30                      ;
+        int number4 = 40                      ;
+        int number5 = 50                      ;
+        int sum                               ;
+        sum = number1 + number2 + number3 + number4 + number5;
+        System.out.print("The sum is ")       ;
+        System.out.println(sum)               ;
+
+        System.out.println();
+        System.out.println();
+
+        int a = 20                            ;
+        int b = 40                            ;
+        int prod                              ;
+        prod = a * b                          ;
+        System.out.print("The product is ")   ;
+        System.out.println(prod)              ;
+
+        System.out.println();
+        System.out.println();
+
+
+        int quot                              ;
+        quot = b / a                          ;
+        float realQuot = (float) b / a        ;
+        System.out.print("The quotient is ")  ;
+        System.out.println(quot)              ;
+
+
+
+
+
+
+
+
+    }
+}
